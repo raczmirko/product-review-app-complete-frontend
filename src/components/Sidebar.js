ae657e2733bb5aacb1e29777612d8d8a74faafc7
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-03-30 20:32:02 +0100

    feat: added icons to sidebar as well as text when bar is opened

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../style/sidebar.css';
+import { CgArrowRightR, CgHome, CgLogIn, CgLogOut } from "react-icons/cg";
 
 const Sidebar = () => {
   const [expanded, setExpanded] = useState(false);
@@ -9,20 +10,27 @@ const Sidebar = () => {
     setExpanded(!expanded);
   };
 
-  return (
-    <div className={`sidebar ${expanded ? 'expanded' : ''}`}>
-      <button onClick={toggleSidebar}>Toggle Sidebar</button>
-      <ul className="sidebar-items">
-        <li>
-          <NavLink to="/" activeClassName="active">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/user/register" activeClassName="active">Register</NavLink>
-        </li>
-        {/* Add more NavLink items for other pages */}
-      </ul>
-    </div>
-  );
+  const sidebarOptions = [
+      { icon: <CgHome />, text: 'Home', route: '/'},
+      { icon: <CgLogIn />, text: 'Login', route: '/' },
+      { icon: <CgLogOut />, text: 'Register', route: 'user/register' }
+    ];
+
+    return (
+        <div className={`sidebar ${expanded ? 'expanded' : ''}`}>
+            <button onClick={toggleSidebar}><CgArrowRightR /></button>
+            <ul className="sidebar-items">
+                {sidebarOptions.map((option, index) => (
+                    <li key={index}>
+                        <NavLink to={option.route} activeClassName="active">
+                            {option.icon}
+                            {expanded && <span> {option.text}</span>}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Sidebar;
