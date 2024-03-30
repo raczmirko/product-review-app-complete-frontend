7fe4182aeda104edca5e8cf41121e681df35d16e
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-03-30 19:11:59 +0100

    refactor: renamed menu to navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../style/navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <h1 className="navbar-title">Product Review Application</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
