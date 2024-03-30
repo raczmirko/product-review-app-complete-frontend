144279ccd942b0d8dbb172da39ad26a355d966f8
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-03-30 21:56:33 +0100

    feat: added logo to navbar and updated CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,11 @@ import '../style/navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
+        {/* This empty div only servers the purpose of pushing the title to the middle
+        Due the the justify:space-between CSS attribute */}
+        <div></div>
         <h1 className="navbar-title">Product Review Application</h1>
+        <img className="navbar-logo" src="icon.png" alt="logo"/>
     </nav>
   );
 };
