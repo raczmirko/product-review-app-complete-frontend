f7994bfc4459604c8a6fcca3ca5a551857a6b639
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-03-30 21:19:36 +0100

    refactor: moved icon to res folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React, { useState } from 'react';
 import Sidebar from './components/Sidebar';
+import './style/styles.css';
 
 const App = () => {
     const [sidebarOpen, setSidebarOpen] = useState(false);
