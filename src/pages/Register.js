43412be30e4b1523aceea0341ec49cf1227bfb86
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-03-31 14:24:39 +0200

    feat: added Notifications to registration page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import '../style/styles.css';
 import ParticleBackground from "../components/ParticleBackground.js";
+import Notification from "../components/Notification.js";
 
 const Register = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [showNotification, setNotification] = useState(false);
+
+    const handleShowNotification = () => {
+        setNotification(true);
+    };
+
+    const handleNotificationClose = () => {
+        setNotification(null);
+    };
 
     const handleRegistration = (e) => {
         e.preventDefault(); // Prevent default form submission behavior
@@ -23,12 +33,16 @@ const Register = () => {
         })
         .then(response => {
             if (!response.ok) {
+                const notificationText = "Registration failed with an error code " + response.status;
+                console.log(response)
+                setNotification({ type: "error", title:"ERROR", text: notificationText});
                 throw new Error('Registration failed');
             }
-            return response.json();
+            else {
+                setNotification({ type: "success", title:"SUCCESS", text: "Registration successful!"});
+            }
         })
         .then(data => {
-            console.log(data);
             // TODO redirect on registration
         })
         .catch(error => console.error('Error:', error));
@@ -41,14 +55,22 @@ const Register = () => {
             <form onSubmit={handleRegistration}>
                 <div className="form-group">
                     <label>Username:</label>
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required/>
                 </div>
                 <div className="form-group">
                     <label>Password:</label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
                 </div>
                 <button type="submit">Register</button>
             </form>
+            {showNotification && (
+                <Notification
+                    title={showNotification.title}
+                    type={showNotification.type}
+                    text={showNotification.text}
+                    onClose={handleNotificationClose}
+                />
+            )}
         </div>
     );
 };
