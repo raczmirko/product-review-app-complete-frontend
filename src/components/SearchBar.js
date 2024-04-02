cc0e7157f364e34e543c572e12e1ac3656099004
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-04-02 21:26:38 +0200

    feat: added advanced search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,52 @@
 import React, { useState } from 'react';
-import { CgSearch, CgDisplayGrid, CgList, CgRedo, CgAddR } from "react-icons/cg";
+import { CgSearch, CgDisplayGrid, CgList, CgRedo, CgAddR, CgMore, CgErase } from "react-icons/cg";
 import '../style/searchbar.css';
 import '../style/styles.css';
 
-const SearchBar = ({ searchFunction, filterList, setFilter }) => {
+const SearchBar = ({ searchFunction, filterList, setFilter, addFunction }) => {
+    const [advancedSearch, setAdvancedSearch] = useState(false);
+
+    const toggleAdvancedSearch = () => {
+        setAdvancedSearch(!advancedSearch);
+    }
 
     return (
-        <div className="search-bar">
-            <div className="search-input">
-                <input onChange={searchFunction} type="text" placeholder="Search..." />
-                <button><CgSearch /></button>
+        <div>
+            <div className="search-bar">
+                <div className="search-input">
+                    <input onChange={searchFunction} type="text" placeholder="Search..." />
+                    <button><CgSearch /></button>
+                </div>
+                <div className="selector search-selector">
+                    <select onChange={(e) => setFilter(filterList[e.target.selectedIndex - 1])}>
+                        <option value="">Select a filter</option>
+                    </select>
+                </div>
+                <div className="search-display">
+                    <button title="Advanced Options" onClick={toggleAdvancedSearch}><CgMore /></button>
+                    <button title="Refresh" className="button-orange" onClick={searchFunction}><CgRedo /></button>
+                    <button title="Add new" className="button-confirm"><CgAddR /></button>
+                </div>
             </div>
-            <div className="selector search-selector">
-                <select onChange={(e) => setFilter(filterList[e.target.selectedIndex - 1])}>
-                    <option value="">Select a filter</option>
-                </select>
-            </div>
-            <div className="search-display">
-                <button><CgDisplayGrid  /></button>
-                <button><CgList /></button>
-                <button className="button-orange" onClick={searchFunction}><CgRedo /></button>
-                <button className="button-confirm"><CgAddR /></button>
-            </div>
+            { advancedSearch &&
+                <div className="search-bar search-bar-advanced">
+                    <div className="selector search-selector">
+                        <select onChange={(e) => setFilter(filterList[e.target.selectedIndex - 1])}>
+                            <option value="">Maximum results per page</option>
+                        </select>
+                    </div>
+                    <div className="selector search-selector">
+                        <select onChange={(e) => setFilter(filterList[e.target.selectedIndex - 1])}>
+                            <option value="">Order by</option>
+                        </select>
+                    </div>
+                    <div className="search-display">
+                        <button title="Reset Filters"><CgErase /></button>
+                        <button title="Display mode: CARDS"><CgDisplayGrid  /></button>
+                        <button title="Display mode: TABLE"><CgList /></button>
+                    </div>
+                </div>
+            }
         </div>
     );
 };
