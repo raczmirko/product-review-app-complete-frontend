62b0af05a6fa9370edacac062610e30ca96f98b3
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-04-02 14:00:10 +0200

    feat: added DynamicTable component that renders a table of an input list of obejcts

---
+++ src/components/DynamicTable.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const DynamicTable = ({ data }) => {
+  if (!data || data.length === 0) {
+    return <p>No data available.</p>;
+  }
+
+  // Extract column names from the keys of the first object
+  const columnNames = Object.keys(data[0]);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {/* Render table headers with column names */}
+          {columnNames.map((columnName, index) => (
+            <th key={index}>{columnName}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {/* Render table rows with data */}
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {columnNames.map((columnName, index) => (
+              <td key={index}>{row[columnName]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default DynamicTable;
