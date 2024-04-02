f8db1d6ddb74033a7a9c7d512f485d472f069af7
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-04-02 17:26:55 +0200

    fix: moved SessionTimer functionality to NavBar to avoid unmounting of timer element (thus resetting timer), fixed accuracy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,56 @@
-import React from 'react';
 import '../style/navbar.css';
-import SessionTimer from './SessionTimer';
+import React, { useState, useEffect } from 'react';
+import { MdOutlineTimer } from "react-icons/md";
 
 const Navbar = ({isLoggedIn, remainingSeconds, logOut}) => {
-  return (
-    <nav className="navbar">
-        <div>
-        {
-            isLoggedIn
-            &&
-            <SessionTimer   isLoggedIn={isLoggedIn}
-                            totalSeconds={remainingSeconds}
-                            functionToRunOnFinish={logOut}
-            />
+    const [formattedTime, setFormattedTime] = useState("");
+    const expiryTime = new Date().getTime() + remainingSeconds * 1000;
+
+    useEffect(() => {
+        if (!isLoggedIn) {
+            // If not logged in, return early without setting up the timer
+            return;
         }
-        </div>
-        <h1 className="navbar-title">Product Review Application</h1>
-        <img className="navbar-logo" src="/icon.png" alt="logo"/>
-    </nav>
+
+        const timer = setInterval(() => {
+            // Get the current time
+            const currentTime = new Date().getTime();
+
+            // Calculate remaining seconds by subtracting the current time from the target time
+            const remainingSeconds = Math.max(Math.floor((expiryTime - currentTime) / 1000), 0);
+
+            // Format remaining seconds into minutes and seconds
+            const formattedMinutes = String(Math.floor(remainingSeconds / 60)).padStart(2, '0');
+            const formattedSeconds = String(remainingSeconds % 60).padStart(2, '0');
+
+            // Update state with the formatted time
+            setFormattedTime(`${formattedMinutes}:${formattedSeconds}`);
+
+            // If remainingSeconds is 0, call functionToRunOnFinish and clear the interval
+            if (remainingSeconds === 0) {
+              logOut();
+              clearInterval(timer);
+            }
+        }, 1000);
+
+        // Clear interval on component unmount
+        return () => clearInterval(timer);
+    }, [remainingSeconds, logOut]);
+
+    return (
+        <nav className="navbar">
+            <div className="session-timer">
+            {
+                isLoggedIn
+                &&
+                <div>
+                <MdOutlineTimer /> {formattedTime}
+                </div>
+            }
+            </div>
+            <h1 className="navbar-title">Product Review Application</h1>
+            <img className="navbar-logo" src="/icon.png" alt="logo"/>
+        </nav>
   );
 };
 
