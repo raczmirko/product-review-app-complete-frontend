2af4100eee1423e57bd44b85a5db320d8cb97065
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-04-02 15:28:29 +0200

    feat: added delete button to DynamicTable.js

--- src/components/DynamicTable.js
+++ src/components/DynamicTable.js
@@ -1,36 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CgTrash } from "react-icons/cg";
 import "../style/table.css";
 
 const DynamicTable = ({ data }) => {
-  if (!data || data.length === 0) {
-    return <p>No data available.</p>;
-  }
+    const [tableData, setTableData] = useState(data);
 
-  // Extract column names from the keys of the first object
-  const columnNames = Object.keys(data[0]);
+      if (!data || data.length === 0) {
+        return <p>No data available.</p>;
+      }
 
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          {/* Render table headers with column names */}
-          {columnNames.map((columnName, index) => (
-            <th key={index}>{columnName}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {/* Render table rows with data */}
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {columnNames.map((columnName, index) => (
-              <td key={index}>{row[columnName]}</td>
+      // Extract column names from the keys of the first object
+      const columnNames = Object.keys(data[0]);
+
+      const handleEdit = (id, field, value) => {
+          // Find the record in the table data array
+          const updatedData = tableData.map(item => {
+            if (item.id === id) {
+              return { ...item, [field]: value };
+            }
+            return item;
+          });
+
+          setTableData(updatedData);
+        };
+
+        const handleDelete = id => {
+            console.log('deleting: ' + id)
+            const updatedData = tableData.filter(item => item.id !== id);
+            setTableData(updatedData);
+        };
+
+      return (
+        <table className="table">
+          <thead>
+            <tr>
+              {/* Render table headers with column names */}
+              {columnNames.map((columnName, index) => (
+                <th key={index}>{columnName}</th>
+              ))}
+                <th key="delete">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Render table rows with data */}
+            {data.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {columnNames.map((columnName, index) => (
+                  <td key={index}>{row[columnName]}</td>
+                ))}
+                <td>
+                  <button className="button-delete" onClick={() => handleDelete(row.id)}><CgTrash /></button>
+                </td>
+              </tr>
             ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+          </tbody>
+        </table>
+      );
 };
 
 export default DynamicTable;
