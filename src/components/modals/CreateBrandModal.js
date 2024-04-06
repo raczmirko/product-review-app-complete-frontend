cdd20f16d8d5ecee9bd146a5c7b609decf26d585
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-04-06 12:11:04 +0200

    feat: added inputs on BrandModal

--- src/components/modals/CreateBrandModal.js
+++ src/components/modals/CreateBrandModal.js
@@ -1,20 +1,42 @@
 import React, { useState } from 'react';
+import CountrySelector from "../CountrySelector";
 import "../../style/modal.css";
+import "../../style/styles.css";
 
-const CreateBrandModal = ({ entityToAdd, closeFunction }) => {
+const CreateBrandModal = ({ entityToAdd, closeFunction, createBrandFunction }) => {
+    const [name, setName] = useState('');
+    const [country, setCountry] = useState({});
+    const [description, setDescription] = useState('');
 
-  const handleClose = () => {
-    closeFunction();
-  };
+    const handleCountrySelect = () => {
 
-  return (
-    <div className="modal-create">
-        <div className="modal-header">{`NEW ${entityToAdd}`}</div>
-        <div className="modal-button" >
-            <button onClick={handleClose}>Close</button>
+    }
+
+    const handleClose = () => {
+        closeFunction();
+    }
+
+    return (
+        <div className="modal-create">
+            <div className="page-header">{`NEW ${entityToAdd}`}</div>
+            <div className="form-group">
+                <label>Username:</label>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} required/>
+            </div>
+            <div className="form-group">
+                <label>Country:</label>
+                <CountrySelector onSelect={handleCountrySelect} />
+            </div>
+            <div className="form-group textarea-container">
+                <label>Description:</label>
+                <textarea value={description} onChange={(e) => setDescription(e.target.value)} required/>
+            </div>
+            <div className="modal-buttons" >
+                <button className="button-confirm" onClick={handleClose}>Create</button>
+                <button className="button-cancel" onClick={handleClose}>Cancel</button>
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default CreateBrandModal;
